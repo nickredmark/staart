5b39fcdef00165f9c0bc33ad5e6fa9dc2ce69771
Nicola Marcacci Rossi <nicolamr@gmail.com>
2017-07-11 15:43:03 +0200

    fix dependencies

--- examples/staart/README.md
+++ examples/staart/README.md
@@ -3,3 +3,7 @@
 Get this folder to start your own project.
 
 Copy `settings.dist.js` to `settings.js` and adapt to your needs.
+
+Build: `yarn`
+
+Dev: `yarn run dev`
--- examples/staart/settings.dist.js
+++ examples/staart/settings.dist.js
@@ -2,7 +2,7 @@ module.exports = {
     url: 'http://localhost',
     port: 3002,
     originUrl: 'http://localhost:3000',
-    mongoUrl: 'mongodb://localhost:27017/blog',
+    mongoUrl: 'mongodb://localhost:27017/staart',
     sharedSecret: 'XXX',
     sessionSecret: 'XXX',
     oothPath: '/auth',
