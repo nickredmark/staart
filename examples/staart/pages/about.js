2c9be9275c0a63bf5010f421e901db87faa212f7
Nicola Marcacci Rossi <nicolamr@gmail.com>
2017-07-15 01:25:26 +0200

    update about

--- examples/staart/pages/about.js
+++ examples/staart/pages/about.js
@@ -8,13 +8,13 @@ export default withPage(() => (
         <p>This is a sample site created with Staart, a lightweight UI components library integrated with Ooth, a user accounts library for node.</p>
         <h3>Philosophy</h3>
         <p>Note: Right now, Staart is a prototype that I built quickly to fulfill my needs. Over time, hopefully, it will improve. What follows is what Staart aims to be.</p>
-        <p>Staart is <b>not a framework</b>. Staart should introduce the least amount of technology coupling possible. Pick and choose the peaces you like and glue them together as you wish. There might be utilities that do the coupling for you, but it should be straightforward to replace them with something else. You should stay in control.</p>
+        <p>Staart is <b>not a framework</b>. Staart should introduce the least amount of technology coupling possible. Pick and choose the pieces you like and glue them together as you wish. There might be utilities that do the coupling for you, but it should be straightforward to replace them with something else. You should stay in control.</p>
         <p>Staart is <b>not a boilerplate</b>. Boilerplates help you get started quickly and allow you to stay in control. But afterwards you are on your own. Plus, boilerplates don't get maintained, because even the creators' code bases diverge from the boilerplates they produced. Ideally, your code base should contain nothing more than business logic.</p>
         <p>Staart is <b>not a code generator</b> for the same reasons it's not a boilerplate.</p>
         <p>What Staart is: <b>a set of components and libraries</b> that can be easily glued together to bootstrap a project, with minimum boilerplate code.</p>
         <h2>About Nick Redmark</h2>
         <p>Hi, I'm Nick Redmark, I'm the developer of these open source projects. I created them mainly for myself, to have a quick way to create new websites that need user accounts. Check out my projects on my <a href="http://nickredmark.com" target="_blank">personal site</a>.</p>
-        <image src="/static/nickredmark.jpg" style={{
+        <img src="/static/nickredmark.jpg" style={{
           maxWidth: '100%',
           width: '200px'
         }}/>
