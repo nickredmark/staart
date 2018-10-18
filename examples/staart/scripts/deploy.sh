ec2786e9108f1a9020f47f0262cb38a78f9f6b02
Nick Redmark <nicolamr@gmail.com>
2018-10-19 01:02:10 +0200

    fix deploy script

--- examples/staart/scripts/deploy.sh
+++ examples/staart/scripts/deploy.sh
@@ -1,7 +1,7 @@
 git push server master
 ssh me@nmr.io << EOF
   set -e
-  cd Documents/staart/staart
+  cd Documents/staart/staart/examples/staart
   git merge master
   npm start
   docker logs -f 
