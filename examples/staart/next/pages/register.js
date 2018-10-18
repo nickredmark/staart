c0b16a0e8da70df2509e4a93abfac30ea2401614
Nick Redmark <nicolamr@gmail.com>
2018-10-19 01:28:06 +0200

    add forgotten param

--- examples/staart/next/pages/register.js
+++ examples/staart/next/pages/register.js
@@ -1,6 +1,11 @@
 import Layout from '../components/layout';
 import withPage from '../providers/page';
 import Register from 'staart/lib/components/register';
+import getConfig from 'next/config';
+
+const {
+  publicRuntimeConfig: { facebookClientId, googleClientId },
+} = getConfig();
 
 export default withPage(({ url: { query: { next } } }) => {
   return (
